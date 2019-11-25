43bf57f87528c64996d26b18537b62114c753fb6
Asia Gulacz <57046956+JoannaGulacz@users.noreply.github.com>
2019-11-25 20:38:29 +0100

    Revert "Revert "adding data inputs""
    This reverts commit f1b1ebf436b2c7a5ff36dbb5702be780b02ce6ee.

--- src/Trump/views/headerView.js
+++ src/Trump/views/headerView.js
@@ -24,9 +24,20 @@ export default class HeaderView extends MainView{
 
     // *** INSERT BOOTSTRAP CLASSES HERE *** //
     // nadpisuje klasy!
-    // this._setAttributes('#startingAddress','class','');
+    // this._setAttributes('startingAddress','class','');
     // this._setAttributes('#destination','class','');
     // this._setAttributes('#searchButton','class','');
+    let currentMonth, currentDay;
+    (new Date().getMonth() < 9) ? currentMonth = `0${new Date().getMonth()+1}` : currentMonth = new Date().getMonth()+1;
+    (new Date().getDate() < 10) ? currentDay = `0${new Date().getDate()}` : currentDay = new Date().getDate();
+    let currentDate = `${new Date().getFullYear()}-${currentMonth}-${currentDay}`;
+    this._setAttributes('dateInput', 'value', currentDate);
+
+    let currentHour, currentMinute;
+    (new Date().getHours() < 10) ? currentHour = `0${new Date().getHours()}` : currentHour = new Date().getHours();
+    (new Date().getMinutes() < 10) ? currentMinute = `0${new Date().getMinutes()}` : currentMinute = new Date().getMinutes();
+    let currentTime = `${currentHour}:${currentMinute}`;
+    this._setAttributes('timeInput', 'value', currentTime);
   }
 
   init(){
