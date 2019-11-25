7ad2a9273d2cfed502a398c15cdb744b8205ab55
Asia Gulacz <57046956+JoannaGulacz@users.noreply.github.com>
2019-11-25 20:37:46 +0100

    adding data inputs

--- src/Trump/models/searchModel.js
+++ src/Trump/models/searchModel.js
@@ -4,5 +4,16 @@ export default class SearchModel {
   constructor() {
     this.start = ''; // domyślnie lokalizacja urządzenia?
     this.meta = '';
+    this.date = '';
+    this.time = '';
+  }
+  takeDateFromInput(){
+    let year = this.date.substring(0,4);
+    let month = this.date.substring(5,7);
+    let day = this.date.substring(8,10);
+    let hour = this.time.substring(0,2);
+    let minutes = this.time.substring(3,5);
+
+    return new Date(year,month-1,day,hour,minutes)
   }
 }
