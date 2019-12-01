e02085c6b96647948d058f50260fbe4c4a25f7bb
Asia Gulacz <57046956+JoannaGulacz@users.noreply.github.com>
2019-12-01 18:40:08 +0100

    creating event

--- src/Trump/models/drivingModel.js
+++ src/Trump/models/drivingModel.js
@@ -8,20 +8,21 @@ class DrivingModel extends BaseModel {
       this.time = "",
       this.dist = 0,
       this.cost = ""
-    this.naviLink = "https://www.google.com/"
+      this.naviLink = "https://www.google.com/"
   }
 
   // w modelach będą funkcje asynchroniczne i pomocnicze do nich aby przekzaywać dane w odpowiednim formacie, na potrzeby testów zwracam zwykły obiekt
   getData() {
     return {
       transport: this.transport,
-      icon: `<i class="fas fa-car"></i>`,
       name: this.name,
       cost: this.cost,
       time: this.time,
       dist: this.dist,
       cost: this.cost,
       naviLink: this.naviLink,
+      duration: this.duration,
+      icon: `<i class="fas fa-car"></i>`
     }
   }
   async update() {
@@ -43,6 +44,7 @@ class DrivingModel extends BaseModel {
     this.dist = Math.round(trans.distance.value / 100)/10;
     this.cost = "?";
     this.naviLink="https://www.google.com/maps/dir/?api=1&origin="+window.origin_place.split(' ').join('+')+"&destination="+window.destination_place.split(' ').join('+')+"&travelmode=driving";
+    this.duration = trans.duration.value;
   }
 }
 
